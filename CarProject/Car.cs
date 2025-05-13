578191c8f71460dd1cd9174cadc53df1a30dac6e
Jasmin May Anderson <05jasmay@gmail.com>
2025-05-13 11:07:54 +0200

    Added code to repository

---
+++ CarProject/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject
+{
+    class Car
+    {
+        private string Model { get; }
+        private string Brand { get; }
+        private int publishedYear { get; }
+        private string Colour { get; }
+        private double horsePower { get; }
+
+        public Car(string model, string brand, int publishedyear, string colour, double horsepower)
+        {
+            Model = model;
+            Brand = brand;
+            publishedYear = publishedyear;
+            Colour = colour;
+            horsePower = horsepower;
+        }
+
+        public void PrintCar()
+        {
+            Console.WriteLine($"Model: {Model}");
+			Console.WriteLine($"Brand: {Brand}");
+			Console.WriteLine($"Published Year: {publishedYear}");
+			Console.WriteLine($"Colour: {Colour}");
+			Console.WriteLine($"Horse Power: {horsePower}");
+		}
+	}
+}
---
+++ CarProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CarProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Model A", "Toyota", 2000, "White", 200);
+
+			car.PrintCar();
+		}
+    }
+}
